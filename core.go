635812162e01d730b8cc8f6a267eff4ccefb995e
dirgadm <dmeligo50@gmail.com>
2021-02-03 07:17:11 +0700

    fix api

--- core.go
+++ core.go
@@ -1,48 +1,59 @@
 package edenrepo
 
 import (
-	"fmt"
-	"io"
 	"log"
 	"net/http"
 )
 
 // CoreGateway ...
-type CoreGateway struct {
-	Client http.Client
+type Auth struct {
+	credentials string
 }
 
 // Call : Base Method to call Core API
-func (client *CoreGateway) Call(method, path string, body io.Reader, v interface{}) error {
-	credentials := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImpveUBlZGVuZmFybS5pZCIsInBhc3N3b3JkIjoiZGV2MTIzIn0.tgiwi4Mwcj5l2oyrCKIpN6BE7dHCO18vqBAnfk1Q4hU"
-	var bearer = "Bearer " + credentials
+//func (client *CoreGateway) Call(method, path string, body io.Reader, v interface{}) error {
+//	credentials := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImpveUBlZGVuZmFybS5pZCIsInBhc3N3b3JkIjoiZGV2MTIzIn0.tgiwi4Mwcj5l2oyrCKIpN6BE7dHCO18vqBAnfk1Q4hU"
+//	var bearer = "Bearer " + credentials
+//
+//	req, err := http.NewRequest(method, path, nil)
+//	if err != nil {
+//		log.Fatalln(err)
+//	}
+//	req.Header.Add("Authorization", bearer)
+//	resp, err := client.Client.Do(req)
+//	if err != nil {
+//		log.Println("Error on response.\n[ERRO] -", err)
+//	}
+//
+//	defer resp.Body.Close()
+//
+//	return err
+//}
 
-	req, err := http.NewRequest(method, path, nil)
+// GetStockTransferByID ...
+func (auth *Auth) GetStockTransferByID(id string) (r http.Response, e error) {
+	if auth.credentials == "" {
+		log.Fatalln("missing or malformed jwt")
+		r.Status = "fail"
+		return
+	}
+	resp := &http.Response{}
+
+	url := baseURL + "/v1/inventory/stock-transfer/" + id + ""
+	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	req.Header.Add("Authorization", bearer)
-	resp, err := client.Client.Do(req)
+
+	req.Header.Add("Authorization", bearer+auth.credentials)
+	client := &http.Client{}
+	resp, e = client.Do(req)
 	if err != nil {
-		log.Println("Error on response.\n[ERRO] -", err)
+		log.Fatalln(e.Error())
+		return
 	}
 
 	defer resp.Body.Close()
 
-	return err
-}
-
-// GetStockTransferByID ...
-func (client *CoreGateway) GetStockTransferByID(id string) (http.Response, error) {
-	resp := http.Response{}
-
-	url := baseURL + "/v1/inventory/stock-transfer/" + id + ""
-	err := client.Call("GET", url, nil, &resp)
-
-	if err != nil {
-		fmt.Println("Failed to get stoct transfer")
-		return resp, err
-	}
-
-	return resp, nil
+	return *resp, nil
 }
--- var.go
+++ var.go
@@ -2,4 +2,5 @@ package edenrepo
 
 const (
 	baseURL = "10.10.0.48:8082"
+	bearer  = "Bearer "
 )
